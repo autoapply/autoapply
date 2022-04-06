d70477334e90095bfe4ca3d6431dbb8b6c6df2d4
Pascal <pascal.gruen@gmail.com>
2022-04-06 21:26:31 +0200

    Update build

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 
+repository="autoapply/autoapply"
+
 build_docker() {
   dockerfile="$1"
-  tag="$2"
-
-  echo "Building $dockerfile -> $tag..."
-  docker build . -f "$dockerfile"
+  version="$2"
+  if [ -n "$version" ]; then
+    tag="$repository:$version"
+    echo "Building $dockerfile -> $tag..."
+    docker build . -f "$dockerfile" -t "$tag" || exit 1
+    docker push "$tag" || exit 1
+  else
+    echo "Building $dockerfile..."
+    docker build . -f "$dockerfile" || exit 1
+    echo "Skipping docker push for branch '$CIRCLE_BRANCH'"
+  fi
 }
+
+echo "${DOCKER_PASSWORD}" |
+  docker login -u "${DOCKER_USERNAME}" --password-stdin || exit 1
 
 if [ -n "$CIRCLE_TAG" ]; then
   build_docker "build/Dockerfile" "$CIRCLE_TAG"
   build_docker "build/kubectl/Dockerfile" "${CIRCLE_TAG}-kubectl"
   build_docker "build/root/Dockerfile" "${CIRCLE_TAG}-root"
-else
+elif [ "$CIRCLE_BRANCH" = "main" ]; then
   build_docker "build/Dockerfile" "latest"
   build_docker "build/kubectl/Dockerfile" "kubectl"
   build_docker "build/root/Dockerfile" "root"
+else
+  build_docker "build/Dockerfile"
+  build_docker "build/kubectl/Dockerfile"
+  build_docker "build/root/Dockerfile"
 fi

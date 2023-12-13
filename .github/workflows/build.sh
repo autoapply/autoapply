d271358b1e6ff873971186e3e13caca3eedc774a
Pascal <pascal.gruen@gmail.com>
2023-12-13 09:12:39 +0100

    Switch to GitHub Actions

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -9,25 +9,25 @@ build_docker() {
     tag="$repository:$version"
     echo "Building $dockerfile -> $tag..."
     docker buildx build --platform=linux/amd64,linux/arm64 . -f "$dockerfile" -t "$tag" || exit 1
-    docker push "$tag" || exit 1
+    echo docker push "$tag" || exit 1
   else
     echo "Building $dockerfile..."
     docker buildx build --platform=linux/amd64,linux/arm64 . -f "$dockerfile" || exit 1
-    echo "Skipping docker push for branch '$CIRCLE_BRANCH'"
+    echo "Skipping docker push for ref '$REF_NAME'"
   fi
 }
 
 echo "${DOCKER_PASSWORD}" |
   docker login -u "${DOCKER_USERNAME}" --password-stdin || exit 1
 
-if [ -n "$CIRCLE_TAG" ]; then
-  build_docker "build/Dockerfile" "$CIRCLE_TAG"
-  build_docker "build/kubectl/Dockerfile" "${CIRCLE_TAG}-kubectl"
-  build_docker "build/root/Dockerfile" "${CIRCLE_TAG}-root"
-elif [ "$CIRCLE_BRANCH" = "main" ]; then
+if [ "$REF_NAME" = "main" ]; then
   build_docker "build/Dockerfile" "latest"
   build_docker "build/kubectl/Dockerfile" "kubectl"
   build_docker "build/root/Dockerfile" "root"
+elif echo "$REF_NAME" | grep -Eq '[0-9]+\.[0-9]+\.[0-9]+'; then
+  build_docker "build/Dockerfile" "$REF_NAME"
+  build_docker "build/kubectl/Dockerfile" "${REF_NAME}-kubectl"
+  build_docker "build/root/Dockerfile" "${REF_NAME}-root"
 else
   build_docker "build/Dockerfile"
   build_docker "build/kubectl/Dockerfile"
